bb8f2c2693b440658e923230e0b95c59e7bc9baa
Jethro Au <supergalong@gmail.com>
2021-09-22 00:42:56 +0100

    update

---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: "center" }}>loading...</h1>
+      <div class="loading">Loading&#8230;</div>
+    </div>
+  );
+};
+
+export default Loading;
